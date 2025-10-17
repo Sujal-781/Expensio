528cd7d2f20c2a177a7f49a1207d0f4818fa466b
Sujal Choudhary <chsujal8@gmail.com>
2025-10-17 23:04:49 +0530

    added first controller and first entity class

---
+++ expensio-backend/src/main/java/in/sujal/expensio/entity/ProfileEntity.java
@@ -0,0 +1,43 @@
+package in.sujal.expensio.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_profiles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullname;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String profileImageUrl;
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private Boolean isActive;
+    private String activationToken;
+
+    @PrePersist //Activates the block of code before storing it to the database
+    public void prePersist(){
+        if(this.isActive == null){
+            isActive = false;
+        }
+    }
+}
