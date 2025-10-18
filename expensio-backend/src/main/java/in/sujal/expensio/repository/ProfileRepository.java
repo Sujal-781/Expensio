b21278d8436e8d82ef5c8015145b80ee412e03da
Sujal Choudhary <chsujal8@gmail.com>
2025-10-18 19:01:33 +0530

    Made the profile activation link work

--- expensio-backend/src/main/java/in/sujal/expensio/repository/ProfileRepository.java
+++ expensio-backend/src/main/java/in/sujal/expensio/repository/ProfileRepository.java
@@ -9,4 +9,5 @@ public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
     //This interface will automatically provide CRUD operations for ProfileEntity
     //We can add custom query methods here, if needed
     Optional<ProfileEntity> findByEmail(String email); //Optional is used to avoid null pointer exception
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
 }
