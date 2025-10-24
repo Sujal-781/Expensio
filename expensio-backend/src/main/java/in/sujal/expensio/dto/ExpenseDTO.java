56b8abd2b68eff522d22398881dc5011e546c66e
Sujal Choudhary <chsujal8@gmail.com>
2025-10-25 00:48:27 +0530

    created dtos for incomes and exoenses

---
+++ expensio-backend/src/main/java/in/sujal/expensio/dto/ExpenseDTO.java
@@ -0,0 +1,24 @@
+package in.sujal.expensio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpenseDTO {
+    private Long id;
+    private String name;
+    private String icon;
+    private String categoryName;
+    private BigDecimal amount;
+    private LocalDateTime date;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
