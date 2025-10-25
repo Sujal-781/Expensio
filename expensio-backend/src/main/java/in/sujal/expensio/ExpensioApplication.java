518397bb0f593b67fbc38983e634ba2307453ec2
Sujal Choudhary <chsujal8@gmail.com>
2025-10-25 16:15:12 +0530

    added daily email service for daily reminders and daily expense summary

--- expensio-backend/src/main/java/in/sujal/expensio/ExpensioApplication.java
+++ expensio-backend/src/main/java/in/sujal/expensio/ExpensioApplication.java
@@ -2,7 +2,9 @@ package in.sujal.expensio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ExpensioApplication {
 
