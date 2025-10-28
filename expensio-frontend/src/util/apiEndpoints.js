ea85a236f6886d8805e76e45ef89f991c8c4dc90
Sujal Choudhary <chsujal8@gmail.com>
2025-10-28 15:44:45 +0530

    endpoint fix

--- expensio-frontend/src/util/apiEndpoints.js
+++ expensio-frontend/src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://expensio-fico.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://expensio-fico.onrender.com/api/v1.0";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "dmpkezxgd";
 
 export const API_ENDPOINTS = {
